7e02ef1d9ba701f914c1f80d49f267610e5a2985
RodrigoAcosta1 <88693866+RodrigoAcosta1@users.noreply.github.com>
2023-10-10 18:25:38 -0300

    Inclusion Htaccess

--- validarLogin.php
+++ validarLogin.php
@@ -53,9 +53,12 @@
 						$_SESSION['codusu1'] = $respuesta[0]["codusu"];
 						$_SESSION['Dependencia1'] = $respuesta[0]["Dependencia"];
 						$_SESSION['Detalle1'] = $respuesta[0]["Detalle"];
+
+						//niveles de usuarios:  1-ADMINISTRADOR || 2-SUPERVISOR || 3-OPERADOR
+ 						$_SESSION['nivel'] = $respuesta[0]["nivel"];
 						$_SESSION['validarIngreso'] = "ok";
 						//Redirecciona a la página principal
-						header("Location:index.php");
+						header("Location:./listaDescargas");
 					}else{
 						echo "<script>	
 								jQuery(function(){
@@ -99,7 +102,7 @@
 									})
 							});
 						</script>";
-				}
+				} //$logueoRta['rta']=='OK'
 
 }else{
 	echo "<script>	
